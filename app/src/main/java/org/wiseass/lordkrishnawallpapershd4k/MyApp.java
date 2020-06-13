706da82eca7c191b90229480f31463d525314235
Raviraj <raviraj.pophale@gmail.com>
2020-06-14 03:44:44 +0530

    first commit

---
+++ app/src/main/java/org/wiseass/lordkrishnawallpapershd4k/MyApp.java
@@ -0,0 +1,14 @@
+package org.wiseass.lordkrishnawallpapershd4k;
+
+import android.app.Application;
+
+public class MyApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+
+    }
+}
